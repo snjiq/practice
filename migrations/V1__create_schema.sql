4cc7b98b1073193ccca00e6c807c37a9b8907d2a
misnjiq <snjiqqq@gmail.com>
2025-10-07 23:43:12 +0300

    Создана структура бд

--- migrations/V1__create_schema.sql
+++ migrations/V1__create_schema.sql
@@ -1,2 +1,28 @@
 -- V1__create_schema.sql
--- Первичная миграция для создания структуры БД
+-- Создание таблиц для управления заказами
+
+DROP TABLE IF EXISTS customers CASCADE;
+DROP TABLE IF EXISTS products CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price NUMERIC(10,2) NOT NULL,
+    stock INT DEFAULT 0
+);
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers(id),
+    product_id INT REFERENCES products(id),
+    quantity INT NOT NULL,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
